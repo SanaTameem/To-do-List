d800dbc855f7cd687f90a7482369b7f84fc26f4e
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-06-08 16:01:47 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-const toDoInput = document.querySelector('.todo-input');
-const toDoListContainer = document.querySelector('.todolist-container');
-const toDoList = document.querySelector('.todo-list');
-const enterBtn = document.querySelector('.enter-btn');
-const arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [
-    {description:' Cleaning', completed:false, index:1},
-    {description:'Reading', completed:false, index:2},
-    {description:'Walking', completed:false, index:3}
-];  
 
-//function display Tasks:
-const display = () =>{
-     arrayOfTasks.forEach(todo => {
-        
-        const element = document.createElement('div');
-        element.classList.add('todo-item');
-        const attr = document.createAttribute('data-id');
-        attr.value = todo.index;
-        element.setAttributeNode(attr);
-        element.innerHTML = `
+const toDoInput = document.querySelector('.todo-input');
+const toDoList = document.querySelector('.todo-list');
+const arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [
+  { description: ' Cleaning', completed: false, index: 1 },
+  { description: 'Reading', completed: false, index: 2 },
+  { description: 'Walking', completed: false, index: 3 },
+];
+
+// function display Tasks:
+const display = () => {
+  arrayOfTasks.forEach((todo) => {
+    const element = document.createElement('div');
+    element.classList.add('todo-item');
+    const attr = document.createAttribute('data-id');
+    attr.value = todo.index;
+    element.setAttributeNode(attr);
+    element.innerHTML = `
                     <div class="check-text">
                         <input type="checkbox" class="larger">
                         <p class="item-text">${todo.description}</p>
                     </div>
                     <i class="fa-solid fa-ellipsis-vertical"></i>
                 `;
-        toDoList.appendChild(element);
-        toDoInput.value = '';
-        localStorage.setItem('arrayOfTasks',JSON.stringify(arrayOfTasks));
-    });
+    toDoList.appendChild(element);
+    toDoInput.value = '';
+    localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
+  });
 };
-//event listeners
-document.addEventListener('DOMContentLoaded',display);
+// event listeners
+document.addEventListener('DOMContentLoaded', display);
