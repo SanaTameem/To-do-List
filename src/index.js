bd56a3728d702dd3f00d2398f13c649cd520651a
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-06-08 15:59:12 +0430

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,73 +1,35 @@
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+const toDoInput = document.querySelector('.todo-input');
+const toDoListContainer = document.querySelector('.todolist-container');
+const toDoList = document.querySelector('.todo-list');
+const enterBtn = document.querySelector('.enter-btn');
+const arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [
+    {description:' Cleaning', completed:false, index:1},
+    {description:'Reading', completed:false, index:2},
+    {description:'Walking', completed:false, index:3}
+];  
 
-class TaskList {
-  constructor() {
-    this.toDoInput = document.querySelector('.todo-input');
-    this.toDoList = document.querySelector('.todo-list');
-    this.enterBtn = document.querySelector('.enter-btn');
-    this.arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [];
-    this.enterBtn.addEventListener('click', this.addTask.bind(this));
-  }
-  // functions
-
-// function display Tasks:
-display = () => {
-  this.arrayOfTasks.forEach((todo) => {
-    // div for to do item
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('todo-item');
-    const attr = document.createAttribute('data-id');
-    attr.value = todo.index;
-    itemDiv.setAttributeNode(attr);
-    // div for checkbox and text
-    const checkTextDiv = document.createElement('div');
-    checkTextDiv.classList.add('check-text');
-    // checbox
-    const checkbox = document.createElement('input');
-    checkbox.classList.add('larger');
-    checkbox.type = 'checkbox';
-    // input for text
-    const inputText = document.createElement('input');
-    inputText.classList.add('item-text');
-    inputText.type = 'text';
-    // adding inputs to the div
-    checkTextDiv.appendChild(checkbox);
-    checkTextDiv.appendChild(inputText);
-    inputText.value = todo.description;
-    // adding the check div to main div
-    itemDiv.appendChild(checkTextDiv);
-    // creat icon:
-    const icon = document.createElement('i');
-    icon.classList.add('fa-solid');
-    icon.classList.add('fa-ellipsis-vertical');
-    // adding icon to div
-    itemDiv.appendChild(icon);
-    this.toDoList.appendChild(itemDiv);
-  });
-}
-
-addTask = () => {
-  const inputValue = this.toDoInput.value;
-  const completed = false;
-  const index = new Date().getTime().toString();
-
-  if (inputValue !== '') {
-    const task = {
-      description: inputValue,
-      completed,
-      index,
-    };
-    this.toDoList.innerHTML = '';
-    this.arrayOfTasks.push(task);
-    // display
-    this.display();
-    this.toDoInput.value = '';
-    localStorage.setItem('arrayOfTasks', JSON.stringify(this.arrayOfTasks));
-  }
-}
-}
-
-// event listeners
-const list = new TaskList();
-document.addEventListener('DOMContentLoaded', list.display);
+//function display Tasks:
+const display = () =>{
+     arrayOfTasks.forEach(todo => {
+        
+        const element = document.createElement('div');
+        element.classList.add('todo-item');
+        const attr = document.createAttribute('data-id');
+        attr.value = todo.index;
+        element.setAttributeNode(attr);
+        element.innerHTML = `
+                    <div class="check-text">
+                        <input type="checkbox" class="larger">
+                        <p class="item-text">${todo.description}</p>
+                    </div>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                `;
+        toDoList.appendChild(element);
+        toDoInput.value = '';
+        localStorage.setItem('arrayOfTasks',JSON.stringify(arrayOfTasks));
+    });
+};
+//event listeners
+document.addEventListener('DOMContentLoaded',display);
