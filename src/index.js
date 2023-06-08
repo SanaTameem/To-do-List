3effbd2f6122449d8d3d1ee5127e0674a1e04b6d
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-06-08 14:11:35 +0430

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,47 @@
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-class TaskList{
-    constructor(){
-        this.toDoInput = document.querySelector('.todo-input');
-        this.toDoList = document.querySelector('.todo-list');
-        this.enterBtn = document.querySelector('.enter-btn');
-        this.arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [];
-        this.enterBtn.addEventListener('click', this.addTask.bind(this));
-    }
-    // functions
+
+class TaskList {
+  constructor() {
+    this.toDoInput = document.querySelector('.todo-input');
+    this.toDoList = document.querySelector('.todo-list');
+    this.enterBtn = document.querySelector('.enter-btn');
+    this.arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [];
+    this.enterBtn.addEventListener('click', this.addTask.bind(this));
+  }
+  // functions
 
 // function display Tasks:
 display = () => {
   this.arrayOfTasks.forEach((todo) => {
-    //div for to do item
+    // div for to do item
     const itemDiv = document.createElement('div');
     itemDiv.classList.add('todo-item');
     const attr = document.createAttribute('data-id');
     attr.value = todo.index;
     itemDiv.setAttributeNode(attr);
-    //div for checkbox and text
+    // div for checkbox and text
     const checkTextDiv = document.createElement('div');
     checkTextDiv.classList.add('check-text');
-    //checbox
+    // checbox
     const checkbox = document.createElement('input');
     checkbox.classList.add('larger');
     checkbox.type = 'checkbox';
-    //input for text
+    // input for text
     const inputText = document.createElement('input');
     inputText.classList.add('item-text');
     inputText.type = 'text';
-    //adding inputs to the div
+    // adding inputs to the div
     checkTextDiv.appendChild(checkbox);
     checkTextDiv.appendChild(inputText);
     inputText.value = todo.description;
-    //adding the check div to main div
+    // adding the check div to main div
     itemDiv.appendChild(checkTextDiv);
-    //creat icon:
+    // creat icon:
     const icon = document.createElement('i');
     icon.classList.add('fa-solid');
     icon.classList.add('fa-ellipsis-vertical');
-    //adding icon to div 
+    // adding icon to div
     itemDiv.appendChild(icon);
     this.toDoList.appendChild(itemDiv);
   });
@@ -65,8 +66,7 @@ addTask = () => {
     localStorage.setItem('arrayOfTasks', JSON.stringify(this.arrayOfTasks));
   }
 }
-};
-
+}
 
 // event listeners
 const list = new TaskList();
