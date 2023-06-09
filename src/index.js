d747de2b79213cf7a45b0dd417dac54bca05c6ea
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-06-09 13:16:49 +0430

    Add new index.js file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 const toDoInput = document.querySelector('.todo-input');
 const toDoList = document.querySelector('.todo-list');
-const arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [
-  { description: ' Cleaning', completed: false, index: 1 },
-  { description: 'Reading', completed: false, index: 2 },
-  { description: 'Walking', completed: false, index: 3 },
-];
+const enterBtn = document.querySelector('.enter-btn');
+let arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [];
 
 // function display Tasks:
 const display = () => {
@@ -17,17 +14,83 @@ const display = () => {
     const attr = document.createAttribute('data-id');
     attr.value = todo.index;
     element.setAttributeNode(attr);
-    element.innerHTML = `
-                    <div class="check-text">
-                        <input type="checkbox" class="larger">
-                        <p class="item-text">${todo.description}</p>
-                    </div>
-                    <i class="fa-solid fa-ellipsis-vertical"></i>
-                `;
+    // div of checkbox and text input
+    const checkTextDiv = document.createElement('div');
+    checkTextDiv.classList.add('check-text');
+    // input for todo text
+    const inputText = document.createElement('input');
+    inputText.type = 'text';
+    inputText.classList.add('item-text');
+    inputText.value = todo.description;
+    // event listener for input text of each to do task
+    inputText.addEventListener('blur', (e) => {
+      const newInputValue = e.target.value;
+      todo.description = newInputValue;
+      localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
+    });
+    // input for checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('larger');
+    // adding checkbox and text to div
+    checkTextDiv.appendChild(checkbox);
+    checkTextDiv.appendChild(inputText);
+
+    // creating font-awesome icon
+    const icon = document.createElement('i');
+    icon.classList.add('fa-solid');
+    icon.classList.add('fa-ellipsis-vertical');
+    if (icon.classList.contains('fa-ellipsis-vertical')) {
+      icon.addEventListener('click', (e) => {
+        const targetIcon = e.target;
+        targetIcon.classList.remove('fa-solid');
+        targetIcon.classList.remove('fa-ellipsis-vertical');
+        targetIcon.classList.toggle('fa-regular');
+        targetIcon.classList.toggle('fa-trash-can');
+        // Event listener for delete icon
+        if (icon.classList.contains('fa-trash-can')) {
+          const deleteIcons = element.querySelectorAll('.fa-trash-can');
+          deleteIcons.forEach((deleteIcon) => {
+            deleteIcon.addEventListener('click', () => {
+              const item = deleteIcon.parentElement;
+              const itemId = item.getAttribute('data-id');
+              toDoList.removeChild(item);
+
+              arrayOfTasks = arrayOfTasks.filter((obj) => obj.index !== parseInt(itemId, 10));
+
+              arrayOfTasks.forEach((task, index) => {
+                task.index = index + 1;
+              });
+
+              localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
+            });
+          });
+        }
+      });
+    }
+
+    // adding div and icon to main div
+    element.appendChild(checkTextDiv);
+    element.appendChild(icon);
     toDoList.appendChild(element);
     toDoInput.value = '';
-    localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
   });
 };
+
+// function addTask
+const addTask = () => {
+  if (toDoInput.value !== '') {
+    const task = {
+      description: toDoInput.value,
+      completed: false,
+      index: arrayOfTasks.length + 1,
+    };
+    toDoList.innerHTML = '';
+    arrayOfTasks.push(task);
+    display();
+    localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
+  }
+};
 // event listeners
+enterBtn.addEventListener('click', addTask);
 document.addEventListener('DOMContentLoaded', display);
